cb9d78abdb387b60ddd5f8c237d4a2fdbab7193b
fastfingertips <fastfingertips@gmail.com>
2024-08-17 19:29:54 +0300

    refactor: improve model definitions

--- models/models.py
+++ models/models.py
@@ -1,10 +1,22 @@
 class Article:
-    def __init__(self, title, link):
+    """Represents an article with a title and a link."""
+    
+    def __init__(self, title: str, link: str):
         self.title = title
         self.link = link
 
+    def __repr__(self) -> str:
+        return f"Article(title={self.title!r}, link={self.link!r})"
+
 class Page:
-    def __init__(self, articles, current_page, total_pages):
+    """Represents a page containing articles and pagination info."""
+    
+    def __init__(self, articles: list[Article], current_page: int, total_pages: int):
         self.articles = articles
         self.current_page = current_page
         self.total_pages = total_pages
+
+    def __repr__(self) -> str:
+        return (f"Page(current_page={self.current_page}, "
+                f"total_pages={self.total_pages}, "
+                f"articles={self.articles!r})")
